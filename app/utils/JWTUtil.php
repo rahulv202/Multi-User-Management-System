4bc4802bd4e4ae783f1336c47cd8a6b536d54de1
Rahul Vaidya <rahulvaidya202@gmail.com>
2024-12-03 13:45:20 +0530

    Create a utility class to generate and validate JWT tokens.

---
+++ app/utils/JWTUtil.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Utils;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTUtil
+{
+    private $secret;
+    private $algorithm;
+    private $expiry;
+
+    public function __construct($config)
+    {
+        $this->secret = $config['secret'];
+        $this->algorithm = $config['algorithm'];
+        $this->expiry = $config['expiry'];
+    }
+
+    public function generateToken($payload)
+    {
+        $payload['iat'] = time();
+        $payload['exp'] = time() + $this->expiry;
+
+        return JWT::encode($payload, $this->secret, $this->algorithm);
+    }
+
+    public function validateToken($token)
+    {
+        try {
+            return JWT::decode($token, new Key($this->secret, $this->algorithm));
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
